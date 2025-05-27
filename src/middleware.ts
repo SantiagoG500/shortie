850660b3d033ea076b968255a0a1bd6a78bc9d97
Santiago Gamboa <santiagogamboa.b500@gmail.com>
2025-05-27 16:47:12 -0500

    fix: prevent redirect loop and avoid auth page redirect when already authenticated

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { isCuid } from '@paralleldrive/cuid2';
 import { NextResponse } from 'next/server'
 import { NextRequest } from 'next/server'
 import { getOneUrlFromDb } from './server/actions/redirect';
-import { privateRoutes, publicRoutes } from './routes';
+import { appDomain, privateRoutes, publicRoutes } from './routes';
 import { auth } from './auth';
 
 
@@ -12,21 +12,30 @@ export async function middleware(request: NextRequest) {
   
   const isPrivateRoute = privateRoutes.includes(pathName)
   const isPublicRoute = publicRoutes.includes(pathName)
+  const isAuthRoute = pathName === '/auth'
   const isNotFound = pathName === '/not-found'
   const slug = pathName.split('/').pop()
 
-  if (isNotFound) return NextResponse.next()
+  if (isNotFound) {
+    return NextResponse.next()
+  }
   
   if (isPrivateRoute && !session) {
-    console.log('not authenticated');
-    
     return NextResponse.redirect(new URL('/auth', request.url))
+  }
+
+  if (isAuthRoute && session) {
+    return NextResponse.redirect(new URL('/', request.url))
   }
 
   if(!isPublicRoute && !isPrivateRoute) {
     if (slug && isCuid(slug)) {
       const dbResult = await getOneUrlFromDb(slug)
       const {link, error, message, timeStamp} = dbResult      
+
+      if (link?.url && link.url === `${appDomain}${slug}`) {
+        return NextResponse.redirect(new URL('/not-found', request.url))
+      }
 
       if (error && message) {
         const notFoundURL = new URL('/not-found', request.url)
