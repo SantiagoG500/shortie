b575b25ab2e429f8e0c2c31f92674902c0caccb6
Santiago Gamboa <santiagogamboa.b500@gmail.com>
2025-05-31 16:28:20 -0500

    feat: add new styles to not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,49 @@
 'use client'
 
+import { Button, Link, button as buttonStyles } from "@heroui/react";
+import NextLink from 'next/link';
+
+
 export default function NotFound() {
 
   return (
     <>
-      <h1>This is not fine</h1>
-      <img
-        src='https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGZjZm95ajVvamZyazhlaGJrZnNkczB1aXoxNnB2dmVrM2hudG50ciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/NTur7XlVDUdqM/giphy.gif'
-        alt='This is fine dog GIF'
-      >  
-      </img>
+      <header className='flex flex-col items-center mt-14 pl-4 pr-4'>
+        <section className='flex flex-col items-center text-center'>
+          <h1 className='text-4xl font-bold tracking-tight inline from-danger-300 to-danger-600 bg-clip-text text-transparent bg-gradient-to-bl'>
+            This is not fine
+          </h1>
+          <p className='text-default-700/90'>Something went wrong, this page wasn't found!</p>
+        </section>
+
+        <div className='
+            mt-5
+            bg-slate-500
+            aspect-video
+            rounded-xl
+            overflow-hidden'
+        >
+          <img
+            className='object-cover object-center'
+            src='https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGZjZm95ajVvamZyazhlaGJrZnNkczB1aXoxNnB2dmVrM2hudG50ciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/NTur7XlVDUdqM/giphy.gif'
+            alt='This is fine dog GIF'
+          />
+        </div>
+
+
+        <Link
+          href='/'
+          as={NextLink}
+          className={buttonStyles({
+            color: 'danger',
+            radius: 'full',
+            variant: 'light',
+            className: 'mt-4 text-md hover:bg-red-400/30 rounded-xl'
+          })}
+        >
+          Start creating links
+        </Link>
+      </header>
     </>
   ) 
 }
