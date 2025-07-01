4677839bae9cf5c0b6affad178fcd2805353baf3
Santiago Gamboa <santiagogamboa.b500@gmail.com>
2025-06-30 20:44:25 -0500

    docs: update documentation and tweak type definitions for tag actions

--- src/server/actions/tag.ts
+++ src/server/actions/tag.ts
@@ -1,35 +1,41 @@
 'use server'
 
-import { auth } from '@/auth'
 import { db } from '@/db/client'
 import { InsertLinksTags, linksTags, SelectTags, tags } from '@/db/db-schemas'
 import { CreateTagSchema, EditLinkSchema } from '@/schemas/schema'
 import { generateCUID2 } from '@/utils/cuid2'
 import { and, eq, inArray } from 'drizzle-orm'
 import { revalidatePath } from 'next/cache'
+import { hasUser } from '@/utils/actions'
+import { SessionErrors, TagErrors } from './types'
 
-enum TagErrors {
-  SESSION_NOT_FOUND = 'User session not found',
-  NOT_VALID_DATA = 'Data is no valid to be sent to the database',
-  SUBMIT_ERROR = 'Error submiting tag data in Database',
-  ADD_TAGS_ERROR = 'Something went wrong adding tags to the created link',
-  QUERY_ERROR = 'Error retrieving tags',
-  UPDATE_ERROR = 'Error updating tags',
-  DELETION_ERROR = 'Error deleting tag data'
-}
-export type TagBaseReturn = { success: boolean, error: TagErrors | ''}
+type TagAndSessionErrorValues = TagErrors | SessionErrors;
+type TagBaseReturn = {
+  success: boolean
+  error: TagAndSessionErrorValues | ''
+};
 
+/**
+ * **Database request (Create a new tag)**
+ * 
+ * - Requires the **user to be authenticated** 
+ * 
+ * @returns a promise that resolves to a {@link TagBaseReturn} object.
+ * @param tagData - Data used to create the new tag, validated using {@link CreateTagSchema}.
+ * 
+ * @see {@link CreateTagSchema}
+ * @see {@link TagBaseReturn}
+ */
 export async function createTag(tagData: CreateTagSchema): Promise<TagBaseReturn> {
   try {
-    const session = await auth()
+    const { session, success: sessionSuccess, error } = await hasUser()
+    const userId = session?.user?.id
     const { success, data } = CreateTagSchema.safeParse(tagData)
     
-    if (!session?.user?.id) {
-      console.log('Session not found');
-
+    if (!session || !sessionSuccess || !userId) {
       return {
         success: false,
-        error: TagErrors.SESSION_NOT_FOUND
+        error
       }
     }
     if (!success) {
@@ -42,15 +48,19 @@ export async function createTag(tagData: CreateTagSchema): Promise<TagBaseReturn
     const fullTagData: SelectTags = {
       title: data.title,
       id: generateCUID2(),
-      userId: session.user.id
+      userId
     }
     
     revalidatePath('/dashboard')
     await db.insert(tags).values(fullTagData)
 
-    return { success: true, error: '' }
+    return {
+      success: true,
+      error: ''
+    }
   } catch (error) {
     console.error('ERROR WHEN CREATING TAG: ', error)
+
     return {
       success: false,
       error: TagErrors.SUBMIT_ERROR
@@ -59,15 +69,26 @@ export async function createTag(tagData: CreateTagSchema): Promise<TagBaseReturn
 }
 
 export type UpdateTagProps = {tagData: SelectTags, newTagData: CreateTagSchema}
+/**
+ * **Database request (Update a tag from a user)**
+ * - Requires the **user to be authenticated** 
+ * 
+ * @returns A promise that resolves to a {@link TagBaseReturn} indicating success or failure.
+ * @param tagData - The current tag data to be updated. See {@link SelectTags}.
+ * @param newTagData - The new data for the tag, validated against {@link CreateTagSchema}.
+ *
+ * @see {@link UpdateTagProps}
+ * @see {@link TagBaseReturn}
+ */
 export async function updateTag({tagData, newTagData}: UpdateTagProps): Promise<TagBaseReturn> {
   try {
-    const session = await auth()
-    const {success} = EditLinkSchema.safeParse(newTagData)
+    const { session, success: sessionSuccess, error } = await hasUser()
+    const { success } = CreateTagSchema.safeParse(newTagData)
     
-    if (!session?.user?.id) { 
+    if (!sessionSuccess || !session) { 
       return {
         success: false,
-        error: TagErrors.SESSION_NOT_FOUND
+        error
       }
     }
 
@@ -97,27 +118,30 @@ export async function updateTag({tagData, newTagData}: UpdateTagProps): Promise<
 }
 
 export type DeleteTagProps = {tag: SelectTags}
-export async function deleteTag({tag}: DeleteTagProps): Promise<TagBaseReturn>  {
-  
+/**
+ * **Database request (Delete a tag from a user)**
+ * 
+ * - Requires the **user to be authenticated** 
+ * 
+ * @returns A promise that resolves to a {@link TagBaseReturn} indicating the result.
+ * @param tag - The tag to delete. See {@link SelectTags}.
+ *
+ * @see {@link TagBaseReturn}
+ */
+export async function deleteTag({ tag }: DeleteTagProps): Promise<TagBaseReturn>  {
   try {
-    const session = await auth()
+    const { session, success, error } = await hasUser()
+    const userId = session?.user?.id
 
-
-    if (!session?.user?.id) {
+    if (!success || !userId) {
       return {
-        success: true ,
-        error: TagErrors.SESSION_NOT_FOUND,
-      }
+        success: false,
+        error
+      } 
     }
 
-    await db.transaction(async (tx) => {
-      await tx.delete(linksTags)
-        .where( eq(linksTags.tagId, tag.id) )
-
-      await tx.delete(tags)
-        .where( eq(tags.id, tag.id) )
-    })
-
+    await db.delete(tags)
+      .where( eq(tags.id, tag.id) )
     
     revalidatePath('/dashboard')
 
@@ -135,28 +159,40 @@ export async function deleteTag({tag}: DeleteTagProps): Promise<TagBaseReturn>  
   }
 }
 
-// actions realted with actions and tags
-export async function addTags({tags, linkId}: {tags: string[], linkId: string}): Promise<TagBaseReturn> {
+/**
+ * **Database request (Adds tags to a link of an authenticated user.)**
+ * 
+ * - Requires the **user to be authenticated** 
+ * 
+ * @returns A promise that resolves to a {@link TagBaseReturn} indicating success or failure.
+  * @param tags - An array of tag IDs to be added to the link.
+ * @param linkId - The ID of the existing link to which tags will be added.
+ *
+ * @see {@link TagBaseReturn}
+ */
+export async function addTags({ tags, linkId }: { tags: string[], linkId: string }): Promise<TagBaseReturn> {
   try {
-    const session = await auth() 
-    if (!session?.user?.id) {
-      console.log('Session not found');  
+    const { session, success, error } = await hasUser()
+    const userId = session?.user?.id
+
+    if (!success || !userId) {
       return {
-        success: false,
-        error: TagErrors.SESSION_NOT_FOUND
-      }
+        success,
+        error
+      } 
     }
-  
-    const linksAndTags: InsertLinksTags[] = tags.map(tagId => ({tagId, linkId}))
-  
+
+    const linksAndTags: InsertLinksTags[] = tags.map(tagId => ({tagId, linkId, userId: userId}))
+
     await db.transaction(async (tx) => {
       await tx.insert(linksTags).values(linksAndTags)
     })
 
     return {
       success: true,
-      error: '',
+      error: ''
     }
+
   } catch (error) {
     console.log('ERROR ADDING TAG', error);
   
@@ -167,27 +203,30 @@ export async function addTags({tags, linkId}: {tags: string[], linkId: string}):
   } 
 }
 
-export type updateLInksTagsProps = {
-  prevTags: string[],
-  newTags: string[],
-  linkId: string
-}
-
-
-export type getTagsReturn = TagBaseReturn & {data?: SelectTags[]}
+export type getTagsReturn = TagBaseReturn & { data?: SelectTags[] }
+/**
+ * **Database request (Gets all tags from a user)**
+ * 
+ * - Requires the **user to be authenticated** 
+ * 
+ * @returns a promise that resolve to an object: {@link getTagsReturn}
+ * 
+ * @see {@link selectedTags}
+ * @see {@link getTagsReturn}
+ */
 export async function getTags(): Promise<getTagsReturn> {
   try {
-     const session = await auth() 
-     if (!session?.user?.id) {
-       console.log('Session not found');  
-       return {
-         success: false,
-         error: TagErrors.SESSION_NOT_FOUND
-       }
-     }
+    const { session, success, error } = await hasUser()
+    const userId = session?.user?.id
+    if (!success || !session || !userId) {
+      return {
+        success,
+        error
+      }
+    }
 
     const result = await db.select()
-      .from(tags).where(eq(tags.userId, session.user.id))
+      .from(tags).where(eq(tags.userId, userId))
 
     return {
       success: true,
@@ -204,57 +243,119 @@ export async function getTags(): Promise<getTagsReturn> {
   }
 }
 
-export async function updateLinksTags ({ prevTags, newTags, linkId }: updateLInksTagsProps): Promise<TagBaseReturn> {
+export type UpdateLInksTagsProps = {
+  prevTags: string[],
+  newTags: string[],
+  linkId: string
+}
+/**
+ * **Database request (Edits tags owned by a user)**
+ * 
+ * - Requires the **user to be authenticated** 
+ * 
+ * @param prevTags - An array of tag IDs that were previously associated with the link.
+ * @param newTags - An array of new tag IDs to associate with the link.
+ * @param linkId - The ID of the link being updated.
+ * @returns A promise that resolves to a {@link TagBaseReturn} indicating success or failure.
+ *
+ * @see {@link UpdateLinksTagsProps}
+ * @see {@link TagBaseReturn}
+ */
+export async function updateLinksTags ({ prevTags, newTags, linkId }: UpdateLInksTagsProps): Promise<TagBaseReturn> {
   try {
-    const session = await auth()
-
-    if (!session?.user?.id) {
-      console.log('Session not found');
+    const { success, session, error } = await hasUser()
+    const userId = session?.user?.id
+    
+    if (!success || !session || !userId) {
+      console.log('NO SESSION FOUND');
+      
       return {
-        success: false,
-        error: TagErrors.SESSION_NOT_FOUND
+        success,
+        error
       }
     }
-
+        
     if (!prevTags) {
       const { error, success } = await addTags({tags: newTags, linkId})
-
-      revalidatePath('/dashboard')
       
-      return { success, error}
+      revalidatePath('/dashboard')
+      return { success, error }
     }
 
     // It defines which tags are going to be removed and which don't
-    const editionTags = {
-      toAdd: newTags.filter( (tag) => !prevTags.includes(tag) ),
-      toDelete: prevTags.filter( (tag) => !newTags.includes(tag) ),
-      toKeep : prevTags.filter( (tag) => newTags.includes(tag) )
-    }
+    const toAdd = newTags.filter(tag => !prevTags.includes(tag))
+    const toDelete = prevTags.filter(tag => !newTags.includes(tag))
+  
+    await db.transaction( async (tx) => {
+      if (toDelete.length > 0) {    
+        await tx.delete(linksTags)  
+          .where(and(
+            eq(linksTags.linkId, linkId),
+            inArray(linksTags.tagId, toDelete)
+          ))
+      }
 
-    await db.transaction(async (tx) => {
-      await tx.delete(linksTags)
-        .where(and(
-          eq(linksTags.linkId, linkId),
-          inArray(linksTags.tagId, editionTags.toDelete)
-        ))
+      if (toAdd.length > 0) {
+        const tagsToAdd = toAdd.map(tagId => ({
+          tagId,
+          linkId,
+          userId
+        }))
 
-      if (editionTags.toAdd.length > 0) {
-        await tx.insert(linksTags)
-          .values(editionTags.toAdd
-            .map( tag => ({tagId: tag,linkId}) )
-          )
+        await tx.insert(linksTags).values(tagsToAdd)
       }
     })
-        
+
     revalidatePath('/dashboard')
     return {success: true, error: ''}
-
+    
   } catch (error) {
     console.error("ERROR EDITING TAG", error);
 
     return {
       success: false,
       error: TagErrors.UPDATE_ERROR
+    }
+  }
+}
+
+/**
+ * **Database request (Deletes all tags from a user)**
+ * 
+ * - Requires the **user to be authenticated** 
+ * 
+ * @returns A promise that resolves to a {@link TagBaseReturn} indicating success or failure.
+ *
+ * @see {@link TagBaseReturn}
+ */
+export async function deleteTags(): Promise<TagBaseReturn> {
+  try {
+    console.log('DELETE TAGS');
+    
+    const { success, session, error } = await hasUser()
+    const userId = session?.user?.id
+
+    if (!success || !session || !userId) {
+      return {
+        success: false,
+        error
+      }
+    }
+    
+    await db.delete(tags)
+      .where( eq(tags.userId, userId) )
+
+    revalidatePath('/dashboard')
+
+    return {
+      success: true,
+      error: ''
+    }
+    
+  } catch (error) {
+    return {
+      success: false,
+      error: TagErrors.DELETION_ERROR
     }
   }
 }
