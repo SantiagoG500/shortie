3a01435e362429d9589061fcf49af7eaf5494444
Santiago Gamboa <santiagogamboa.b500@gmail.com>
2025-06-30 20:49:48 -0500

    feat: add new action to delete a user account

--- src/server/actions/user.ts
+++ src/server/actions/user.ts
@@ -3,7 +3,15 @@
 import { db } from '@/db/client';
 import { InsertUser, users } from '@/db/db-schemas';
 import { CreateAuthSchema } from '@/schemas/schema';
+import { hasUser } from '@/utils/actions';
 import { eq } from 'drizzle-orm';
+import { SessionErrors, UserErrors } from './types';
+
+type UserAndSessionErrorValues = UserErrors | SessionErrors
+type userBaseReturn = {
+  success: boolean
+  error: UserAndSessionErrorValues | ''
+}
 
 export async function createUser(authData: CreateAuthSchema) {
   try {
@@ -27,4 +35,40 @@ export async function getUser(authData: CreateAuthSchema): Promise<InsertUser | 
       console.log('Error when searching for a user', error)
       return null
     }
+}
+
+/**
+ * **Database request (deletes an user)**
+ * 
+ * - Requires the **user to be authenticated**
+ * 
+ * @returns a promise that resolve to an object: {@link userBaseReturn}
+ * @see {@link userBaseReturn}
+ */
+export async function deleteUser(): Promise<userBaseReturn> {
+  try {
+    const { session, error, success: sessionSuccess } = await hasUser()
+    const userId = session?.user?.id
+
+    if (!session || !sessionSuccess || !userId) {
+      return {
+        success: false,
+        error: SessionErrors.SESSION_NOT_FOUND
+      }
+    }
+    
+    await db.delete(users)
+      .where( eq(users.id, userId) )
+
+    return {
+      success: true,
+      error: ''
+    }
+
+  } catch (error) {
+    return {
+      success: false,
+      error: UserErrors.NOT_FOUND
+    }
+  }
 }
